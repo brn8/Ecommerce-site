0a9817affa8de3c164bd45c90b417d7336f92465
Hey <brn8@njit.edu>
2025-01-06 10:26:11 -0500

    update files for products and cart page

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import "./App.css";
-import Product from "./Product";
+import { useState, useEffect } from "react";
+import "../App.css";
+import Product from "../Product";
 import CartButton from "./CartButton";
 import { Route, Routes } from "react-router-dom";
 import Cart from "./Cart";
@@ -8,13 +8,21 @@ import Cart from "./Cart";
 function App() {
   const [cart, setCart] = useState(true);
   const [cartItem, setCartItem] = useState([]);
+  useEffect(() => {
+    const currentOrder = async () => {
+      const response = await fetch("/api/orderItem");
+      const fetchOrderItem = await response.json();
+      setCartItem(fetchOrderItem);
+    };
+    currentOrder();
+  }, [cart]);
   return (
     <Routes>
       <Route
         path="/"
         element={
           <>
-            <CartButton cart={cart} setCartItem={setCartItem} />
+            <CartButton cart={cart} cartItem={cartItem} />
             <Product cart={cart} setCart={setCart} />
           </>
         }
