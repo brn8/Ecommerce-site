4c8441b39b982f40112c57d47391cd90426ccd19
Hey <brn8@njit.edu>
2025-01-14 15:49:06 -0500

    edit and delete functionality for adminPortal

--- src/Components/AdminPortal.jsx
+++ src/Components/AdminPortal.jsx
@@ -11,6 +11,8 @@ const AdminPortal = ({
   setSearch,
 }) => {
   const [newProduct, setNewProduct] = useState(false);
+  const [refetchData, setRefetchData] = useState(0);
+  const [edit, setEdit] = useState(0);
   const [productName, setProductName] = useState("");
   const [productImg, setProductImg] = useState("");
   const [productDescription, setProductDescription] = useState("");
@@ -19,6 +21,7 @@ const AdminPortal = ({
   const [productDiscountAmount, setProductDiscountAmount] = useState("");
   const [productCategory, setProductCategory] = useState("");
   const [products, setProducts] = useState([]);
+
   const navigate = useNavigate();
 
   const createNewProductsHandler = async (e) => {
@@ -65,16 +68,107 @@ const AdminPortal = ({
     }
   };
 
+  const selectedItem = (product) => {
+    setProducts((prevProducts) => {
+      const updatedProducts = prevProducts.map((p) => {
+        if (p.id === product.id) {
+          return { ...p, isChecked: !p.isChecked };
+        }
+        return p;
+      });
+      return updatedProducts;
+    });
+  };
+
+  const deleteProducts = async () => {
+    await Promise.all(
+      products.map(async (p) => {
+        if (p.isChecked) {
+          await fetch(`/api/products/${p.id}`, {
+            method: "DELETE",
+          });
+        }
+      })
+    );
+    setRefetchData(!refetchData);
+  };
+
+  const editProducts = () => {
+    const selectedProduct = products.filter((product) => product.isChecked);
+    if (selectedProduct.length > 1) {
+      alert("Please select one product at a time to edit");
+    } else {
+      setNewProduct(true);
+      setProductName(selectedProduct[0].name);
+      setProductImg(selectedProduct[0].img);
+      setProductDescription(selectedProduct[0].description);
+      setProductPrice(selectedProduct[0].price);
+      setProductQuantity(selectedProduct[0].quantity);
+      setProductDiscountAmount(selectedProduct[0].discountAmount);
+      setProductCategory(selectedProduct[0].productCategoryName);
+      setEdit(selectedProduct.map((product) => product.id));
+    }
+  };
+
+  const EditProductsHandler = async () => {
+    try {
+      const editProducts = await fetch(`/api/products/${edit[0]}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: productName,
+          img: productImg,
+          description: productDescription,
+          price: productPrice,
+          quantity: parseInt(productQuantity),
+          discountAmount: productDiscountAmount,
+          productCategoryName: productCategory,
+          modified_at: new Date(),
+        }),
+      });
+
+      if (
+        productName === "" ||
+        productImg === "" ||
+        productDescription === "" ||
+        productPrice === "" ||
+        productQuantity === "" ||
+        productDiscountAmount === "" ||
+        productCategory === ""
+      ) {
+        alert("Please fill in all the fields");
+      } else {
+        setProductName("");
+        setProductImg("");
+        setProductDescription("");
+        setProductPrice("");
+        setProductQuantity("");
+        setProductDiscountAmount("");
+        setProductCategory("");
+        setNewProduct(false);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
     if (!newProduct) {
       const fetchProducts = async () => {
         const response = await fetch("/api/product");
         const fetchProduct = await response.json();
-        setProducts(fetchProduct);
+        setProducts(
+          fetchProduct.map((product) => ({
+            ...product,
+            isChecked: false,
+          }))
+        );
       };
       fetchProducts();
     }
-  }, [newProduct]);
+  }, [newProduct, refetchData]);
 
   return (
     <div>
@@ -163,19 +257,52 @@ const AdminPortal = ({
             <br />
             <div className="shipping-button">
               <div>
-                {" "}
-                <button onClick={() => setNewProduct(false)}>Cancel</button>
-              </div>
-              <div>
-                <button onClick={createNewProductsHandler}>Add</button>
+                {edit ? (
+                  <>
+                    <button
+                      onClick={() => {
+                        setProductName("");
+                        setProductImg("");
+                        setProductDescription("");
+                        setProductPrice("");
+                        setProductQuantity("");
+                        setProductDiscountAmount("");
+                        setProductCategory("");
+                        setNewProduct(false);
+                        setEdit(0);
+                      }}
+                    >
+                      Cancel
+                    </button>
+                    <button onClick={EditProductsHandler}>Edit</button>
+                  </>
+                ) : (
+                  <>
+                    <button onClick={() => setNewProduct(false)}>Cancel</button>
+                    <button onClick={createNewProductsHandler}>Add</button>
+                  </>
+                )}
               </div>
             </div>
           </div>
         ) : (
           <>
-            <button onClick={() => setNewProduct(true)}>
-              + Add New Products
-            </button>
+            <div>
+              {products.filter((p) => p.isChecked).length > 0 ? (
+                <>
+                  {products.filter((p) => p.isChecked).length == 1 ? (
+                    <button onClick={editProducts}>Edit Products</button>
+                  ) : (
+                    ""
+                  )}
+                  <button onClick={deleteProducts}>- Delete Products</button>
+                </>
+              ) : (
+                <button onClick={() => setNewProduct(true)}>
+                  + Add New Products
+                </button>
+              )}
+            </div>
 
             <table>
               <tbody>
@@ -192,7 +319,12 @@ const AdminPortal = ({
                 {products.map((product, index) => (
                   <tr key={index}>
                     <td>
-                      <input type="checkbox" style={{ width: "20px" }} />
+                      <input
+                        type="checkbox"
+                        checked={product.isChecked}
+                        style={{ width: "20px" }}
+                        onClick={() => selectedItem(product)}
+                      />
                     </td>
                     <td>
                       <img
